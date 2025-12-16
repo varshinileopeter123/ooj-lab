f84c69147cd134d72a51b786b7f52edc2aa6f225
varshinileopeter123 <varshinileopeter.cs24@bmsce.ac.in>
2025-12-16 22:03:50 +0530

    Create Quadratic.java

---
+++ Quadratic.java
@@ -0,0 +1,39 @@
+QUESTION-1:
+
+import java.util.Scanner;
+
+public class QuadraticEquation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read coefficients
+        System.out.print("Enter value of a: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter value of b: ");
+        double b = sc.nextDouble();
+
+        System.out.print("Enter value of c: ");
+        double c = sc.nextDouble();
+
+        // Calculate discriminant
+        double discriminant = b * b - 4 * a * c;
+
+        // Check nature of roots
+        if (discriminant < 0) {
+            System.out.println("There are no real solutions.");
+        } else if (discriminant == 0) {
+            double root = -b / (2 * a);
+            System.out.println("There is one real solution:");
+            System.out.println("x = " + root);
+        } else {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            System.out.println("There are two real solutions:");
+            System.out.println("x1 = " + root1);
+            System.out.println("x2 = " + root2);
+        }
+
+        sc.close();
+    }
+}
